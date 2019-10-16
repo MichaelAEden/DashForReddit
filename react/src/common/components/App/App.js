80d48ab27b742a7ed0b50c3388e4ccfffb8a7d50
Michael Eden <Mikey.Eden@gmail.com>
2019-10-15 20:21:07 -0400

    Add SubredditNav component

--- react/src/common/components/App/App.js
+++ react/src/common/components/App/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import SubredditNav from '../../../subredditNav';
+
 export default function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <SubredditNav />
+    </div>
+  );
 }
---
+++ react/src/subredditNav/components/SubredditNav/SubredditNav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SubredditNav.css';
+
+export default function SubredditNav(props) {
+  return (
+    <div className="SubredditNav">
+      <p>Hello world!</p>
+    </div>
+  );
+}
